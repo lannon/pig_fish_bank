cca22c57b1d2dba7d45a778ce3c80ccd426b0f38
lannon <jlannon@gmail.com>
2011-06-18 11:26:48 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+## People
+# | Person | Pin  | Balance |
+# | Alice  | 7643 | 2000    |
+# | Bob    | 5954 | 22400   |  
+# | Clyde  | 1562 | 100     |
+alice, bob, clyde = Customer.create(
+  [{:name => "Alice", :pin => 7643},
+  {:name => "Bob", :pin => 5954},
+  {:name => "Clyde", :pin => 1562}]
+)
+alice.accounts.create(:account_type => 'checking', :balance => 2000)
+bob.accounts.create(:account_type => 'checking', :balance => 22400)
+clyde.accounts.create(:account_type => 'checking', :balance => 100)
+
+Account.system.update_attribute(:balance, 10000)
